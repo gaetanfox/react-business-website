2bc183c4d81d5c20d33b995a993cb467cbd14458
Gaetan Fox <bgloup@gmail.com>
2020-09-26 19:27:00 +0200

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './App.css'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path='/' />
       </Switch>
+      <HeroSection />
     </Router>
   )
 }
