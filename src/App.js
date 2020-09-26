3356e543a9833a056594460062d2daa37d3772d0
Gaetan Fox <bgloup@gmail.com>
2020-09-26 21:55:19 +0200

    Footer and routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './components/pages/HomePage/Home'
+import Services from './components/pages/Services/Services'
 import Footer from './components/pages/Footer/Footer'
+import Products from './components/pages/Products/Products'
+import SignUp from './components/pages/SignUp/SignUp'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
       </Switch>
       <Footer />
     </Router>
---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import Pricing from '../../Pricing'
+import { homeObjOne } from './Data'
+
+function Services() {
+  return (
+    <>
+      <Pricing />
+      <HeroSection {...homeObjOne} />
+    </>
+  )
+}
+
+export default Services
