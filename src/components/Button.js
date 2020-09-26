db2a38185d181391d754b15320235f54e0b89c47
Gaetan Fox <bgloup@gmail.com>
2020-09-26 19:15:03 +0200

    Mobile menu and CSS corrections

--- src/components/Button.js
+++ src/components/Button.js
@@ -20,7 +20,7 @@ export const Button = ({
 
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
-  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : COLOR[0]
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null
 
   return (
     <button
