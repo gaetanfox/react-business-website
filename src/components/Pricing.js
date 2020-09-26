dbd391928b08cdec516d87086ed9aeee27662a81
Gaetan Fox <bgloup@gmail.com>
2020-09-26 22:05:10 +0200

    Forgot a title

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -13,7 +13,7 @@ function Pricing() {
       <div>
         <div className='pricing__section'>
           <div className='pricing__wrapper'>
-            <h1 className='pricing__heading'></h1>
+            <h1 className='pricing__heading'>Pricing</h1>
             <div className='pricing__container'>
               <Link to='/sign-up' className='pricing__container-card'>
                 <div className='pricing__container-cardInfo'>
